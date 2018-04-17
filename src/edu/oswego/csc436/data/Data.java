8275b480162df13dd79969491930823218df96d7
Joshua Karns <jkarns275@gmail.com>
2018-04-16 20:40:23 -0400

    Added headers for value methods

--- src/edu/oswego/csc436/data/Data.java
+++ src/edu/oswego/csc436/data/Data.java
@@ -1,6 +1,10 @@
 package edu.oswego.csc436.data;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Data {
+  public static final int SPEED_LIMIT = 196;
+
   private final static Data instance = new Data();
 
   public final UltraSonicSensor uss = UltraSonicSensor.getInstance();
@@ -11,6 +15,9 @@ public class Data {
   private int leftEncoderValue = 0, lastLeftEncoderValue = 0;
   private int rightEncoderValue = 0, lastRightEncoderValue = 0;
 
+  private float rightSteeringCorrectionAverage = 0.0f;
+  private float leftSteeringCorrectionAverage = 0.0f;
+
   Data() { }
 
   public static Data getInstanace() { return Data.instance; }
@@ -18,5 +25,49 @@ public class Data {
   public Data update() {
     // Calculate new values
     return this;
+  }
+
+  public float getLeftSCMD() {
+    return 0;
+  }
+
+  public float getRightSCMD() {
+    return 0;
+  }
+
+  public float getLeftRotationalSpeed() {
+    return 0;
+  }
+
+  public float getRightRotationalSpeed() {
+    return 0;
+  }
+
+  public float rightSteeringCorrection() {
+    float calculatedValue = throw new NotImplementedException();
+    return updateRightSteeringCorrectionAverage(calculatedValue);
+  }
+
+  /**
+   * Update the rolling exponential average
+   * @param newValue
+   * @return the new rolling exponential average
+   */
+  private float updateRightSteeringCorrectionAverage(float newValue) {
+    throw new NotImplementedException();
+  }
+
+  public float getLeftSteeringCorrection() {
+    float calculatedValue = throw new NotImplementedException();
+    return updateLeftSteeringCorrectionAverage(calculatedValue);
+  }
+
+  /**
+   * Update the rolling exponential average
+   * @param newValue
+   * @return the new rolling exponential average
+   */
+  private float updateLeftSteeringCorrectionAverage(float newValue) {
+    throw new NotImplementedException();
   }
 }
