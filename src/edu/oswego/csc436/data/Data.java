ca19bbead9f935a3665d53eb72909ae134f226a6
Joshua Karns <jkarns275@gmail.com>
2018-04-16 20:51:03 -0400

    Added headers for data updating code.

--- src/edu/oswego/csc436/data/Data.java
+++ src/edu/oswego/csc436/data/Data.java
@@ -15,8 +15,15 @@ public class Data {
   private int leftEncoderValue = 0, lastLeftEncoderValue = 0;
   private int rightEncoderValue = 0, lastRightEncoderValue = 0;
 
+  private float rightRotationalSpeed = 0;
+  private float leftRotationalSpeed = 0;
+  private float relativeSpeed = 0.0f;
   private float rightSteeringCorrectionAverage = 0.0f;
   private float leftSteeringCorrectionAverage = 0.0f;
+  private float rightSteeringCorrection = 0.0f;
+  private float leftSteeringCorrection = 0.0f;
+  private float leftSCMD = 0;
+  private float rightSCMD = 0;
 
   Data() { }
 
@@ -24,50 +31,108 @@ public class Data {
 
   public Data update() {
     // Calculate new values
+    this.leftSCMD = this.updateLeftSCMD();
+    this.rightSCMD = this.updateRightSCMD();
+    this.rightRotationalSpeed = this.updateRightRotationalSpeed();
+    this.leftRotationalSpeed = this.updateLeftRotationalSpeed();
+    this.relativeSpeed = this.updateRelativeSpeed();
+    this.rightSteeringCorrectionAverage = this.updateRightSteeringCorrection();
+    this.rightSteeringCorrection  = this.updateRightSteeringCorrection();
+    this.leftSteeringCorrection = this.updateLeftSteeringCorrection();
     return this;
   }
 
-  public float getLeftSCMD() {
+  private float updateRelativeSpeed() {
     return 0;
   }
 
-  public float getRightSCMD() {
+  private float updateLeftSCMD() {
     return 0;
   }
 
-  public float getLeftRotationalSpeed() {
+  private float updateRightSCMD() {
     return 0;
   }
 
-  public float getRightRotationalSpeed() {
+  private float updateLeftRotationalSpeed() {
     return 0;
   }
 
-  public float rightSteeringCorrection() {
+  private float updateRightRotationalSpeed() {
+    return 0;
+  }
+
+  private float updateRightSteeringCorrection() {
     float calculatedValue = throw new NotImplementedException();
     return updateRightSteeringCorrectionAverage(calculatedValue);
   }
 
-  /**
-   * Update the rolling exponential average
-   * @param newValue
-   * @return the new rolling exponential average
-   */
   private float updateRightSteeringCorrectionAverage(float newValue) {
     throw new NotImplementedException();
   }
 
-  public float getLeftSteeringCorrection() {
+  private float updateLeftSteeringCorrection() {
     float calculatedValue = throw new NotImplementedException();
     return updateLeftSteeringCorrectionAverage(calculatedValue);
   }
 
-  /**
-   * Update the rolling exponential average
-   * @param newValue
-   * @return the new rolling exponential average
-   */
   private float updateLeftSteeringCorrectionAverage(float newValue) {
     throw new NotImplementedException();
+  }
+
+  private float updateRightSteeringCorrectionAverage() {
+    return rightSteeringCorrectionAverage;
+  }
+
+  private float updateLeftSteeringCorrectionAverage() {
+    return leftSteeringCorrectionAverage;
+  }
+
+  public int getRightEncoderValue() {
+    return rightEncoderValue;
+  }
+
+  public int getLeftEncoderValue() {
+    return leftEncoderValue;
+  }
+
+  public int getUssValue() {
+    return ussValue;
+  }
+
+  public float getRightSteeringCorrectionAverage() {
+    return rightSteeringCorrectionAverage;
+  }
+
+  public float getLeftSteeringCorrectionAverage() {
+    return leftSteeringCorrectionAverage;
+  }
+
+  public float getRelativeSpeed() {
+    return relativeSpeed;
+  }
+
+  public float getLeftSCMD() {
+    return leftSCMD;
+  }
+
+  public float getRightSCMD() {
+    return rightSCMD;
+  }
+
+  public float getLeftRotationalSpeed() {
+    return leftRotationalSpeed;
+  }
+
+  public float getRightRotationalSpeed() {
+    return rightRotationalSpeed;
+  }
+
+  public float getRightSteeringCorrection() {
+    return rightSteeringCorrection;
+  }
+
+  public float getLeftSteeringCorrection() {
+    return leftSteeringCorrection;
   }
 }
