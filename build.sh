b4866bb2b305f15cb55029c69f8221f555695ea0
Josh Karns <jkarns@oswego.edu>
2018-04-19 11:14:19 -0400

    Added manifest, added build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-javac -cp GoPiGoLib.jar src/edu/oswego/csc436/data/*.java src/edu/oswego/csc436/states/*.java src/edu/oswego/csc436/*.java
-
+javac -d bin -cp GoPiGoLib.jar src/edu/oswego/csc436/data/*.java src/edu/oswego/csc436/states/*.java src/edu/oswego/csc436/*.java
+cd bin
+jar cmvf META-INF/MANIFEST.MF ../out.jar edu/oswego/csc436/*.class edu/oswego/csc436/data/*.class edu/oswego/csc436/states/*.class
